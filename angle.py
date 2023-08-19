cc1f4dc9a99684ca330f1321ed276eae03543fbd
hozturk20 <hozturk20@ku.edu.tr>
2023-08-20 01:13:49 +0300

    single file calculate

--- angle.py
+++ angle.py
@@ -1,39 +1,50 @@
-import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import math
 
 def calculate_angle(point1, point2):
     # Calculate the angle in degrees between two points
     x1, y1 = point1
     x2, y2 = point2
-
     delta_x = x2 - x1
     delta_y = y2 - y1
 
     angle_radians = math.atan2(delta_y, delta_x)
-    angle_degrees = math.degrees(angle_radians)
+    angle_degrees = -math.degrees(angle_radians)
 
-    return angle_degrees
+    return int(angle_degrees)
 
 def main(image_path):
     # Load the image
-    image = cv2.imread(image_path)
-    cv2.imshow('Image', image)
+    img = mpimg.imread(image_path)
 
-    # Wait for the user to click two points
-    print("Click on two points in the image to calculate the angle.")
+    # Display the image
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.imshow(img)
+    plt.title("Click on two points in the image to calculate the angle.")
+
+    # Initialize variables for clicked points
     points = []
 
-    def click_event(event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            points.append((x, y))
+    def onclick(event):
+        nonlocal points
+
+        if event.xdata is not None and event.ydata is not None:
+            points.append((event.xdata, event.ydata))
+
+            # Display a cursor at the clicked point
+            ax.plot(event.xdata, event.ydata, 'rs', markersize=5)
+            plt.draw()
+
             if len(points) == 2:
                 angle = calculate_angle(points[0], points[1])
                 print(f"Angle between points: {angle:.2f} degrees")
-                cv2.destroyAllWindows()
+                plt.close()
 
-    cv2.setMouseCallback('Image', click_event)
-    cv2.waitKey(0)
+    fig.canvas.mpl_connect('button_press_event', onclick)
+    plt.show()
+
 
 if __name__ == "__main__":
-    image_path = "./diffusion/diffusion_voxels/curated"  # Replace with your image file path
+    image_path = "./diffusion/diffusion_voxels/curated/0fa_200_450.png"  # Replace with your image file path
     main(image_path)
